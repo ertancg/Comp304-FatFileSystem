35bf7327349a633bdae630781ad9d9d3dc81070f
ertancg <72572878+ertancg@users.noreply.github.com>
2022-08-24 22:08:08 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Comp304-FatFileSystem
+MINI FILE SYSTEM.
+
+This is an file system API which operates on a virtual disk.
+-mini_fat_save(fs): Writes the metadata of the system and files in to the 'physical' file on the system according the abstraction we've implemented.
+-mini_fat_load(fs): Reads from the actual data file and constructs the needed data structes of system we've designed.
+-mini_fat_write_in_block(FAT_FILESYSTEM *fs, const int block_id, const int block_offset, const int size, const void * buffer): Takes an offset and block number to write data on the system.
+-mini_fat_read_in_block(FAT_FILESYSTEM *fs, const int block_id, const int block_offset, const int size, const void * buffer): Takes an offset and block number to read the data from the system.
+-mini file seek(fs, open file, offset, from start): Changes the position pointer of the open file descriptor. Returns false if the change is not bound to constraints. 
+-mini file delete(fs, filename): Removes the files reference from the system.
+-mini file write(fs, open file, size, buffer): Continues to write from the position of the open file descriptor. Returns the total number of bytes written.
+-mini file read(fs, open file, size, buffer): Continues to read from the position of the open file descriptor. Returns the total number of bytes read.
+How to Run:
+1. First compile the program with running command "make".
+2. Then enter the command "./minifs" to run the mini file system.
--- fat.cpp
+++ fat.cpp
@@ -7,6 +7,8 @@
 #include "fat.h"
 #include "fat_file.h"
 
+#include <unistd.h>
+
 
 /**
  * Write inside one block in the filesystem.
@@ -18,6 +20,7 @@
  * @return              written byte count
  */
 int mini_fat_write_in_block(FAT_FILESYSTEM *fs, const int block_id, const int block_offset, const int size, const void * buffer) {
+	//printf("offset: %d, size: %d, file: %d, buff: %s, buffer size: %d\n", block_offset, size, fs->block_size, buffer, sizeof(buffer));
 	assert(block_offset >= 0);
 	assert(block_offset < fs->block_size);
 	assert(size + block_offset <= fs->block_size);
@@ -25,7 +28,12 @@ int mini_fat_write_in_block(FAT_FILESYSTEM *fs, const int block_id, const int bl
 	int written = 0;
 
 	// TODO: write in the real file.
-
+	FILE *fd = fopen(fs->filename, "r+b");
+	fseek(fd, (block_id * fs->block_size) + block_offset, SEEK_SET);
+	if(fwrite(buffer, size, 1, fd) > 0){
+		written = size;
+	}
+	fclose(fd);
 	return written;
 }
 
@@ -46,7 +54,12 @@ int mini_fat_read_in_block(FAT_FILESYSTEM *fs, const int block_id, const int blo
 	int read = 0;
 
 	// TODO: read from the real file.
-
+	FILE *fd = fopen(fs->filename, "r+");
+	fseek(fd, (block_id * fs->block_size) + block_offset, SEEK_SET);
+	if(fread(buffer, size, 1, fd) > 0){
+		read = size;
+	}
+	fclose(fd);
 	return read;
 }
 
@@ -57,7 +70,11 @@ int mini_fat_read_in_block(FAT_FILESYSTEM *fs, const int block_id, const int blo
  */
 int mini_fat_find_empty_block(const FAT_FILESYSTEM *fat) {
 	// TODO: find an empty block in fat and return its index.
-
+	for(int i = 0; i < fat->block_map.size(); i++){
+		if(fat->block_map[i] == EMPTY_BLOCK){
+			return i;
+		} 
+	}
 	return -1;
 }
 
@@ -113,6 +130,9 @@ FAT_FILESYSTEM * mini_fat_create(const char * filename, const int block_size, co
 	FAT_FILESYSTEM * fat = mini_fat_create_internal(filename, block_size, block_count);
 
 	// TODO: create the corresponding virtual disk file with appropriate size.
+    FILE *fd = fopen(filename, "wb");
+    ftruncate(fileno(fd), block_size*block_count);
+    fclose(fd);
 	return fat;
 }
 
@@ -126,26 +146,60 @@ FAT_FILESYSTEM * mini_fat_create(const char * filename, const int block_size, co
  * @return     true on success
  */
 bool mini_fat_save(const FAT_FILESYSTEM *fat) {
-	FILE * fat_fd = fopen(fat->filename, "r+");
+	FILE * fat_fd = fopen(fat->filename, "rb+");
 	if (fat_fd == NULL) {
 		perror("Cannot save fat to file");
 		return false;
 	}
 	// TODO: save all metadata (filesystem metadata, file metadata).
-
+	
+	fwrite(&(fat->block_count), sizeof(int), 1, fat_fd);
+	fwrite(&(fat->block_size), sizeof(int), 1, fat_fd);
+	fwrite(&(fat->block_map), sizeof(fat->block_map), 1, fat_fd);
+    int file_index = 0;
+    for(int i = 0; i < fat->block_map.size(); i++){
+        if(fat->block_map[i] == FILE_ENTRY_BLOCK){
+            FAT_FILE* fd = fat->files[file_index];
+            int size = fd->size;
+            int name_lenght = strlen(fd->name);
+            fseek(fat_fd, (fd->metadata_block_id * fat->block_size), SEEK_SET);
+            fwrite(&size, sizeof(int), 1, fat_fd);
+            fwrite(&name_lenght, sizeof(int), 1, fat_fd);
+            fwrite(&(fd->name), name_lenght, 1, fat_fd);
+            fwrite(&(fd->block_ids), sizeof(fd->block_ids), 1, fat_fd);
+            file_index++;
+        }
+    }
+	fclose(fat_fd);
 	return true;
 }
 
 FAT_FILESYSTEM * mini_fat_load(const char *filename) {
-	FILE * fat_fd = fopen(filename, "r+");
+	FILE * fat_fd = fopen(filename, "rb+");
 	if (fat_fd == NULL) {
 		perror("Cannot load fat from file");
 		exit(-1);
 	}
 	// TODO: load all metadata (filesystem metadata, file metadata) and create filesystem.
 
-	int block_size = 1024, block_count = 10;
-	FAT_FILESYSTEM * fat = mini_fat_create_internal(filename, block_size, block_count);
-
+    int blocksize;
+    int blockcount;
+	fread(&(blockcount), sizeof(int), 1, fat_fd);
+	fread(&(blocksize), sizeof(int), 1, fat_fd);
+    FAT_FILESYSTEM * fat = mini_fat_create_internal(filename, blocksize, blockcount);
+	fread(&(fat->block_map), sizeof(fat->block_map), 1, fat_fd);
+    for(int i = 0; i < fat->block_map.size(); i++){
+        if(fat->block_map[i] == FILE_ENTRY_BLOCK){
+            FAT_FILE* fd = new FAT_FILE;
+            int name_size;
+            fseek(fat_fd, (i * fat->block_size), SEEK_SET);
+            fread(&(fd->size), sizeof(int), 1, fat_fd);
+            fread(&name_size, sizeof(int), 1, fat_fd);
+            fread(&(fd->name), name_size, 1, fat_fd);
+            fread(&(fd->block_ids), sizeof(fd->block_ids), 1, fat_fd);
+            fat->files.push_back(fd);
+        }
+    }
+	fclose(fat_fd);
 	return fat;
 }
